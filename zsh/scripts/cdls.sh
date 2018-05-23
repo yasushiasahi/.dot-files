006b0f488e677ac8278744aeaa1f75ecdadfbe5b
Yasushi Asahi <asahi1600@gmail.com>
2018-05-24 05:18:53 +0900

    modify shell scripts 'mdcd' and 'cdls'

--- zsh/scripts/cdls.sh
+++ zsh/scripts/cdls.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/zsh
 
-\cd $_
+\cd $_				# cdの前に\を付けないとエイリアス名がcdなのでまた自分を呼び出して延々にループする
 if [ $? = 0 ]; then
-    # echo 'succse'
     echo -e '\e[36m success \e[m'
     ls
 fi
--- zsh/scripts/mdcd.sh
+++ zsh/scripts/mdcd.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/zsh
 
-mkdir $_
-cd $_
+option=''				    # 変数optionを''として初期化
+new_directory_name=$_		            # 変数'new_directory_name'にコマンドの最後の引数を代入
+
+if [ $1 = '-p' ]; then		            # $1(第一引数)が'-p'なら
+    option='-p'			            # 変数opstionの中身を'-p'に変える)
+fi				            # if文終了
+
+mkdir ${option} ${new_directory_name}	    # ディレクトリを作成 第一引数がならpオプションが付く
+
+if [ $? = 0 ]; then	                    # エラーが出なければ(直前の処理が [成功 $? = 0] [失敗 $? = 1])
+    builtin cd ${new_directory_name}	    # 作成したディレクトリに素(buildin)のcdする。（cdだとエイリアスが呼ばれる）
+    echo -e '\e[36m success \e[m'           # 色(36)を付けて'success'を出力
+fi				            # if文終了
